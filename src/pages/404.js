bccdb8d836d6c3e4418d7d58c0922b3a9114c61c
Krishnakant Rawat <krisrawat26@gmail.com>
2023-04-05 20:40:38 +0530

    init + routing

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>What da fuk you lookin fo</div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/sagar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Sagar() {
+  return (
+    <div>Sagar is sexy
+        <br />
+        <Link href='/'>See KK</Link>
+    </div>
+  )
+}
+
+export default Sagar
